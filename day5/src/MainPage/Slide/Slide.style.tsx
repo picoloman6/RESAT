8660ce7f93bfc6e868d2ac3c8d96319041f6b5e4
picoloman6 <a2328b@naver.com>
2023-09-15 13:49:58 +0900

    [Day5] : 슬라이드 이미지 삽입

--- day5/src/MainPage/Slide/Slide.style.tsx
+++ day5/src/MainPage/Slide/Slide.style.tsx
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 
 export const SlideWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 30%;
-  border: 1px solid black;
+  margin-top: 50px;
+  border-bottom: 1px solid #e9e9e9;
+
+  > img {
+    width: 80%;
+    height: 80%;
+  }
 `;
--- day5/src/MainPage/Slide/Slide.tsx
+++ day5/src/MainPage/Slide/Slide.tsx
@@ -1,7 +1,11 @@
 import { SlideWrapper } from './Slide.style';
 
 const Slide = () => {
-  return <SlideWrapper>슬라이드</SlideWrapper>;
+  return (
+    <SlideWrapper>
+      <img src='../../tree.jpg' alt='나무사진' />
+    </SlideWrapper>
+  );
 };
 
 export default Slide;
