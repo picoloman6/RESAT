f05feedc75fc6265ba8aacc3a8b4f995db2dbd43
picoloman6 <a2328b@naver.com>
2023-09-18 16:24:28 +0900

    [Day6] : 슬라이드 사진 서버에서 가져오도록 수정

--- day5/src/MainPage/Slide/Slide.tsx
+++ day5/src/MainPage/Slide/Slide.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import { SlideWrapper } from './Slide.style';
 
 const Slide = () => {
+  const [photos, setPhotos] = useState<string[]>([]);
+
+  const getPhotos = async () => {
+    const response = await axios.get(`http://localhost:3001/photos`);
+    setPhotos(response.data);
+  };
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
   return (
     <SlideWrapper>
-      <img src='../../tree.jpg' alt='나무사진' />
+      {photos &&
+        photos.map((photo, i) => <img src={photo} key={i} alt='사진' />)}
     </SlideWrapper>
   );
 };
