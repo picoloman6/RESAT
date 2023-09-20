710b3652392981a3ca2e9a2013ec144c5207c958
picoloman6 <a2328b@naver.com>
2023-09-20 18:28:53 +0900

    [Day8] : items 모듈 파일 작성

---
+++ day5/modules/items.ts
@@ -0,0 +1,28 @@
+const GET_ITEMS = 'items/GET_ITEMS' as const;
+
+export const getItems = (items: object[]) => ({
+  type: GET_ITEMS,
+  payload: items
+});
+
+type ItemAction = ReturnType<typeof getItems>;
+
+type ItemState = { items: object[] };
+
+const initialState = {
+  items: []
+};
+
+function itemState(
+  state: ItemState = initialState,
+  action: ItemAction
+): ItemState {
+  switch (action.type) {
+    case 'items/GET_ITEMS':
+      return { items: action.payload };
+    default:
+      return state;
+  }
+}
+
+export default itemState;
